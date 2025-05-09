433dcd654e36b5df68c1c58dbeac466fd3b770a5
Debottam <debottammandal445@gmail.com>
2025-05-09 22:32:26 +0530

    changes

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,9 @@
-// app/not-found.tsx
-const NotFound = () => {
+// app/not-found.js
+export default function NotFound() {
     return (
-      <div className="flex h-screen items-center justify-center">
-        <div className="text-center">
-          <h1 className="text-4xl font-bold">404</h1>
-          <p className="text-lg text-gray-500">Page not found</p>
-        </div>
+      <div>
+        <h2>Page Not Found</h2>
+        <p>Could not find the requested resource</p>
       </div>
     );
 }
-  
-export default NotFound 
